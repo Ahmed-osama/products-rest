003d4b23a9de4888cf945de9ac84dfe838a31f4b
Ahmed-osama <ahmed.osama@tajawal.com>
2019-09-04 09:58:31 +0400

    chore: project setup watch files & loggig & err handling

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan')
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
+
+
+const app = express();
+app.use(morgan('dev'))
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error(req.url + ' is Not Found');
+    res.status = 404;
+    next(error)
+})
+app.use((err, req, res, next) => {
+
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            status: err.status,
+            message: err.message
+        }
+    })
+    next(error)
+})
 module.exports = app
