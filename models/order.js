71b08846a7c49941b08ce89f2de5f0b523b045cb
Ahmed-osama <ahmed.osama@tajawal.com>
2019-09-05 11:01:40 +0400

    feat: format response

--- models/order.js
+++ models/order.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
+    name: String,
+    price: Number,
 })
 
 module.exports = mongoose.model('Product', productSchema)
--- utils/product.utils.js
+++ utils/product.utils.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const Product = require('./../models/product');
 
-const createProduct = (product) => new Product({
+const createProduct = product => new Product({
     _id: new mongoose.Types.ObjectId(),
     name: product.name,
     price: product.price
